9905e3794126ecb1c7832c0511ba70a8aeda2d12
Andrei <8700andreihidalgo@gmail.com>
2025-11-25 02:04:22 +0800

    remove unused

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,14 +2,12 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import es from "./locales/es.json";
-import fr from "./locales/fr.json";
 import tl from "./locales/tl.json";
 
 i18next.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     es: { translation: es },
-    fr: { translation: fr },
     tl: { translation: tl },
   },
   lng: localStorage.getItem("language") || "en",
